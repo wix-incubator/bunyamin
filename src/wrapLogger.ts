ccc5c8085b6aedcc899ee0d98c1bef82a88362b7
Yaroslav Serhieiev <noomorph@gmail.com>
2023-06-05 14:54:38 +0300

    feat: add wrapLogger(logger[, options]) signature

--- src/wrapLogger.ts
+++ src/wrapLogger.ts
@@ -5,6 +5,22 @@ export type * from './decorator';
 
 export function wrapLogger<Logger extends BunyanLikeLogger>(
   options: BunyaminConfig<Logger>,
+): Bunyamin<Logger>;
+export function wrapLogger<Logger extends BunyanLikeLogger>(
+  logger: Logger,
+  options?: Omit<BunyaminConfig<Logger>, 'logger'>,
+): Bunyamin<Logger>;
+export function wrapLogger<Logger extends BunyanLikeLogger>(
+  maybeLogger: any,
+  maybeConfig?: unknown,
 ): Bunyamin<Logger> {
-  return new Bunyamin(options);
+  const logger = (maybeLogger.logger ?? maybeLogger) as Logger;
+  const config = (logger === maybeLogger ? maybeConfig : maybeLogger) as
+    | BunyaminConfig<Logger>
+    | undefined;
+
+  return new Bunyamin({
+    ...config,
+    logger,
+  });
 }
