4c9a128e093e383f395990625b22da11b38b533d
Yaroslav Serhieiev <noomorph@gmail.com>
2023-11-04 12:39:17 +0200

    feat: improved global instance

--- src/decorator/types/BunyaminConfig.ts
+++ src/decorator/types/BunyaminConfig.ts
@@ -1,3 +1,4 @@
+import type { ThreadGroupConfig } from '../../streams';
 import type { BunyaminLogRecordFields } from './BunyaminLogRecordFields';
 import type { BunyanLikeLogger } from './BunyanLikeLogger';
 
@@ -6,6 +7,14 @@ export type BunyaminConfig<Logger extends BunyanLikeLogger> = {
    * Underlying logger to be used.
    */
   logger: Logger;
+  /**
+   * Forbids changing the logger after constuction.
+   */
+  immutable?: boolean;
+  /**
+   * Thread groups to be used for grouping log records.
+   */
+  threadGroups?: ThreadGroupConfig[];
   /**
    * Fallback message to be used when there was no previous message
    * passed with {@link BunyaminLogMethod#begin}.
